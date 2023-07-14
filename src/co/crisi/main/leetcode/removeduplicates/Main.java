222c6df0b14c48bdd430a317cbbcc754060387e4
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-07-14 15:44:17 -0500

    added remove duplicates from leetcode

--- src/co/crisi/main/leetcode/removeduplicates/Main.java
+++ src/co/crisi/main/leetcode/removeduplicates/Main.java
@@ -1,2 +1,32 @@
-package co.crisi.main.leetcode.removeduplicates;public class Main {
+package co.crisi.main.leetcode.removeduplicates;
+
+import java.util.Arrays;
+import java.util.Hashtable;
+
+public class Main {
+
+    public static int removeDuplicates(int[] nums) {
+
+        var table = new Hashtable<Integer, Integer>();
+        var k = 0;
+        for(int i=0; i<nums.length; i++){
+            if(!table.containsKey(nums[i])){
+                table.put(nums[i], 1);
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,2};
+
+        int k = removeDuplicates(nums);
+        System.out.println(k);
+        System.out.println(Arrays.toString(nums));
+
+    }
+
 }

57a9824b1514da592066113bf2bb0307ec32af01
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-18 16:28:38 -0500

    passed solution

--- src/co/crisi/main/leetcode/validpattern/Main.java
+++ src/co/crisi/main/leetcode/validpattern/Main.java
@@ -1,69 +1,51 @@
 package co.crisi.main.leetcode.validpattern;
 
-import java.util.Hashtable;
+import java.util.Stack;
 
 public class Main {
 
     static class Solution {
 
         public boolean isValid(String s) {
-            var table = fillTable(s.toCharArray());
 
-            return table.values().stream().allMatch(e -> e.equals(0));
-        }
+            Stack<Character> a = new Stack<>();
+            var array = s.toCharArray();
 
-        public Hashtable<Character, Integer> fillTable(char[] pattern) {
-            var table = new Hashtable<Character, Integer>();
-
-            for (int i = 0; i < pattern.length; i++) {
-                char c = pattern[i];
-
-                switch (c) {
-                    case '(':
-                    case '[':
-                    case '{':
-                        if (table.containsKey(c)) {
-                            table.replace(c, table.get(c) + 1);
-                        } else {
-                            table.put(c, 1);
-                        }
-                        break;
-                    case ')':
-                        char c1 = '(';
-                        if (table.containsKey(c1)) {
-                            table.replace(c1, table.get(c1) - 1);
-                        } else {
-                            table.put(c1, -1);
-                        }
-                        break;
-                    case ']':
-                        char c2 = '[';
-                        if (table.containsKey(c2)) {
-                            table.replace(c2, table.get(c2) - 1);
-                        } else {
-                            table.put(c2, -1);
-                        }
-                        break;
-                    case '}':
-                        char c3 = '{';
-                        if (table.containsKey(c3)) {
-                            table.replace(c3, table.get(c3) - 1);
-                        } else {
-                            table.put(c3, -1);
-                        }
-                        break;
+            for (int i = 0; i < array.length; i++) {
+                char c = array[i];
+                if (c == ')') {
+                    if (!a.isEmpty() && a.peek() == '(') {
+                        a.pop();
+                    }else{
+                        a.push(c);
+                    }
+                } else if (c == ']') {
+                    if (!a.isEmpty() && a.peek() == '[') {
+                        a.pop();
+                    }else{
+                        a.push(c);
+                    }
+                } else if (c == '}') {
+                    if (!a.isEmpty() && a.peek() == '{') {
+                        a.pop();
+                    }else{
+                        a.push(c);
+                    }
+                } else {
+                    a.push(c);
                 }
-
             }
-            return table;
+
+            return a.isEmpty();
         }
+
 
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.isValid("()[]{}"));
+        System.out.println(s.isValid("[](){}()}{"));
 
     }
 
